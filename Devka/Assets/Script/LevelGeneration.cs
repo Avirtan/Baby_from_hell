d75e37fc0ca4b461ce4b2a234c4b59012456d733
Mosha <48620999+MOXHATKA@users.noreply.github.com>
2019-08-16 22:34:42 +0300

    Map Generation
    random blocks

---
+++ Devka/Assets/Script/LevelGeneration.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startingPositions;
+    public GameObject[] rooms; // index 0 - LR, 1 - LRB, 2 - LRT, 3 - LRTB
+
+    private int direction;
+    public float moveAmount;
+
+    private float timeBtwRoom;
+    public float startTimeBtwRoom = 0.25f;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public bool stopGeneration;
+
+    public LayerMask room;
+
+    private int downCounter = 0;
+
+    void Start()
+    {
+        int randStartingPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+    }
+
+    private void Move()
+    {
+        if(direction == 1 || direction == 2)
+        {
+            //move right
+            downCounter = 0;
+            if (transform.position.x < maxX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                //ограничение повторного нанесения комнаты
+                direction = Random.Range(1, 6);
+                if (direction == 3)
+                {
+                    direction = 2;
+                }
+                else if (direction == 4)
+                {
+                    direction = 5;
+                }
+            }
+            else
+            {
+                direction = 5;
+            }
+            
+        }
+        else if (direction == 3 || direction == 4)
+        {
+            //move left
+            downCounter = 0;
+            if (transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(3, 6);
+            }
+            else
+            {
+                direction = 5;
+            }
+            
+        }
+        else if (direction == 5)
+        {
+            //move down
+            downCounter++;
+            if (transform.position.y > minY)
+            {
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+                if (roomDetection.GetComponent<RoomType>().type != 1 && roomDetection.GetComponent<RoomType>().type != 3)
+                {
+                    if (downCounter >= 1)
+                    {
+                        roomDetection.GetComponent<RoomType>().RoomDestruction();
+                        Instantiate(rooms[3], transform.position, Quaternion.identity);
+                    }
+                    else
+                    {
+                        int randBottomRoom = Random.Range(1, 4);
+                        if (randBottomRoom == 2)
+                        {
+                            randBottomRoom = 1;
+                        }
+                        Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
+                    }
+                    roomDetection.GetComponent<RoomType>().RoomDestruction();
+                }
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+                transform.position = newPos;
+
+                int rand = Random.Range(2, 4);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 6);
+            }
+            else
+            {
+                //stop level generation
+                stopGeneration = true;
+            }
+            
+        }
+
+        //Instantiate(rooms[0], transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        if (timeBtwRoom <= 0 && stopGeneration == false)
+        {
+            Move();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+            timeBtwRoom -= Time.deltaTime;
+        }
+    }
+}
