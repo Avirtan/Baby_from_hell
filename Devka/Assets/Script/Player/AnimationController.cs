38e3cc35d882b8996ed1e9180def5393fc6e7c79
Avirtan <36964697+Avirtan@users.noreply.github.com>
2019-08-21 16:33:19 +0300

    Runing

---
+++ Devka/Assets/Script/Player/AnimationController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones;
+
+public class AnimationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string idleAnimation = "idle";
+
+	public string runAnimation = "running";
+
+	public string jumpAnimation = "Jump";
+	public string glideAnimation = "Glide";
+
+	public string dieAnimation = "Die";
+
+	public string attackAnimation = "Attack";
+
+    enum State {IDLE, RUN,SHOOT,DEAD};
+    private State state = State.IDLE;
+
+    private UnityArmatureComponent armatureComponent;
+	private DragonBones.AnimationState aimState = null;
+
+	void Start () {
+		armatureComponent = GetComponent<UnityArmatureComponent>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    // Методы для анимации
+    public void Idle(int direction) {
+        if(state != State.DEAD){
+            if(direction == 1){
+                if (state != State.IDLE) {
+                    armatureComponent.animation.FadeIn (idleAnimation, 0.5f, -1);
+                    armatureComponent.animation.timeScale = 1f;
+                    state = State.IDLE;
+		        }
+                armatureComponent.armature.flipX = false;
+            }else 
+            {
+                if (state != State.IDLE) {
+                    armatureComponent.animation.FadeIn (idleAnimation, 0.5f, -1);
+                    armatureComponent.animation.timeScale = 1f;
+                    state = State.IDLE;
+		        }
+                armatureComponent.armature.flipX = true;
+            }
+        }
+		
+	}
+
+    public void Run(float speed) {
+        if(state !=State.DEAD){
+            if(speed > 0){
+                if(state != State.RUN){
+                    armatureComponent.animation.FadeIn (runAnimation, 0f, -1);
+			        armatureComponent.animation.timeScale = 1f;
+			        state = State.RUN;
+                }
+                armatureComponent.armature.flipX = false;
+            }else{
+                if(state != State.RUN){
+                    armatureComponent.animation.FadeIn (runAnimation, 0f, -1);
+			        armatureComponent.animation.timeScale = 1f;
+			        state = State.RUN;
+                }
+                armatureComponent.armature.flipX = true;
+            }
+        }
+	}
+
+    public void Flip(){
+        armatureComponent.armature.flipX = true;
+    }
+}
---
+++ Devka/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AnimationController))]
+public class PlayerController : MonoBehaviour
+{
+    public Transform groundCheck; // зона снизу
+
+
+    protected AnimationController sprite;
+    private Vector3 speed;
+    private Rigidbody2D player;
+    private float moveX = 0;
+    private int diraction = 1;
+    private bool isDeath = false;
+    private bool onGround = false;
+    private string TagGround = "GROUND";
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<AnimationController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!isDeath){
+            HandlerMove();
+        }
+        ControlAnimation();
+        Debug.Log(isRuning());
+    }
+
+    void HandlerMove(){
+        moveX = Input.GetAxis ("Horizontal");
+        speed = new Vector3 (moveX * 10, player.velocity.y, 0f);
+        diraction = moveX != 0 ?(moveX>0?diraction = 1:diraction=-1):diraction;
+        player.velocity = speed;
+    }
+
+    void ControlAnimation(){
+        if(isDeath){
+            return;
+        }else if(OnGround()){
+            if(isRuning()){
+                sprite.Run(moveX);
+            }else{
+                sprite.Idle(diraction);
+            }
+        }
+    }
+
+    bool isRuning(){
+        return moveX == 0?false:true;
+    }
+
+    bool OnGround(){
+        onGround = Physics2D.Linecast(transform.position, groundCheck.position,(1<<10));
+        return onGround;
+    }
+
+
+    /*
+    protected virtual void OnCollisionStay2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag(TagGround)) {
+			//onGround = true;
+		}
+	}
+	protected virtual void OnCollisionExit2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag (TagGround)) {
+			//onGround = false;
+		}
+	}
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag(TagGround)) {
+			//onGround = true;
+		}
+    } */
+}
