f422edb6f9c32f05dee3b14a9ad4900af2d82104
Avirtan <36964697+Avirtan@users.noreply.github.com>
2019-08-25 19:54:32 +0300

    Devka3d

--- Devka/Assets/Script/Player/Controller.cs
+++ Devka/Assets/Script/Player/Controller.cs
@@ -9,18 +9,22 @@ public class Controller : MonoBehaviour
 
     private float MoveX = 0;
     private Rigidbody rgb3d;
-    private Vector3 velocity;
+    private Vector3 velocity = new Vector3(0,0,0);
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rgb3d = GetComponent<Rigidbody>();
+        rgb3d.velocity = new Vector3(0,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInput();
+        isWall();
+        isGround();
     }
 
     void FixedUpdate(){
@@ -37,5 +41,26 @@ public class Controller : MonoBehaviour
 
     private void GetInput(){
         MoveX = Input.GetAxis ("Horizontal");
+    }
+
+
+    private float lenghtToGround = 1.5f; // Расстояние до земли
+    private bool isGround(){
+       Debug.DrawLine(rgb3d.position,new Vector3(rgb3d.position.x,rgb3d.position.y-lenghtToGround,rgb3d.position.z),Color.red,2f);
+       return Physics.Raycast(rgb3d.position, Vector3.down, lenghtToGround);
+    }
+
+    //Расстояние до правой и левой стены
+    private float lenghtToWallLeft = 0.6f;
+    private float lenghtToWallRight = 0.6f;
+    private bool isWall(){
+        bool left = Physics.Raycast(rgb3d.position, Vector3.left, lenghtToWallLeft);
+        bool right = Physics.Raycast(rgb3d.position, Vector3.right, lenghtToWallRight);
+        Debug.DrawLine(rgb3d.position,new Vector3(rgb3d.position.x-lenghtToWallLeft,rgb3d.position.y,rgb3d.position.z),Color.red,1f);
+        Debug.Log("leftWall"+left);
+        Debug.Log("Right"+right);
+        Debug.DrawLine(rgb3d.position,new Vector3(rgb3d.position.x+lenghtToWallRight,rgb3d.position.y,rgb3d.position.z),Color.red,1f);
+        if(left || right) return true;
+        else return false;
     }
 }
